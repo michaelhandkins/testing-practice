4974530b2da49a0a6030478d473ef7faecda6656
Michael Handkins <michaelhandkins@gmail.com>
2020-11-10 11:06:38 -0600

    Initial commit

---
+++ Just Start Testing/ValidationService.swift
@@ -0,0 +1,9 @@
+//
+//  ValidationService.swift
+//  Just Start Testing
+//
+//  Created by Michael Handkins on 11/10/20.
+//  Copyright © 2020 Kilo Loco. All rights reserved.
+//
+
+import Foundation
