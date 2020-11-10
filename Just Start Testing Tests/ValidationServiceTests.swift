8780d4066dc750beb616844a606ec509f9a60d13
Michael Handkins <michaelhandkins@gmail.com>
2020-11-10 11:31:24 -0600

    Create test unit bundle and validation service tests file

---
+++ Just Start Testing Tests/ValidationServiceTests.swift
@@ -0,0 +1,15 @@
+//
+//  ValidationServiceTests.swift
+//  Just Start Testing Tests
+//
+//  Created by Michael Handkins on 11/10/20.
+//  Copyright © 2020 Kilo Loco. All rights reserved.
+//
+
+import XCTest
+
+class ValidationServiceTests: XCTestCase {
+
+    
+
+}
